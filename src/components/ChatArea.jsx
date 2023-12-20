779cae971cbae20885f8be666de11903762bd093
unknown <heroglasses155@gmail.com>
2023-12-20 09:59:04 +0630

    changed into sendable bytecode

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -1,13 +1,40 @@
-import { Button, Input, Upload } from "antd";
+import { Button, Upload } from "antd";
 import { UploadOutlined, SendOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
 const ChatArea = () => {
   const [fileList, setFileList] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const uploadButton = <Button icon={<UploadOutlined />}></Button>;
 
-  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+  const handleChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+    // Store the selected file in the component's state
+    if (newFileList.length > 0) {
+      setSelectedFile(newFileList[0].originFileObj);
+    }
+  };
+
+  const convertFileToBytecode = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = reject;
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleSendClick = async () => {
+    if (selectedFile) {
+      const bytecode = await convertFileToBytecode(selectedFile);
+      // Now you can use the 'bytecode' for further processing or send it to your server
+      console.log(bytecode);
+    }
+  };
+
   return (
     <div>
       <div className="w-[300px] border-2 border-red-500 h-[300px] mb-2">
@@ -26,10 +53,12 @@ const ChatArea = () => {
           <Button
             disabled={fileList.length < 1}
             icon={<SendOutlined />}
+            onClick={handleSendClick}
           ></Button>
         </div>
       </div>
     </div>
   );
 };
+
 export default ChatArea;
